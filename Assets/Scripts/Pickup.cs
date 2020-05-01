b2b215b5b8468ac977d8ef1b7bd113e5082c8b5d
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-05-01 19:01:42 -0400

    outline shader

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pickup : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Pickup : MonoBehaviour {
+
+    void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
