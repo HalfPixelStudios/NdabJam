b6779a3262266c29d7bf7be43045128a1f4943eb
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-05-03 12:37:31 -0400

    lvl 3 progress

--- Assets/Scripts/Slots/SwitchSlot.cs
+++ Assets/Scripts/Slots/SwitchSlot.cs
@@ -6,11 +6,19 @@ public class SwitchSlot : Slot {
 
     public GameObject reverseableConveyerRoot;
     bool toggle = true;
+    public Animator anim;
 
+    private void Start() {
+        base.Start();
+
+    }
     public override void Interact(Slot playerSlot) {
         toggle = !toggle;
 
         ReversibleConveryerSlot.ReverseDirection(reverseableConveyerRoot);
+        anim.Play("greenSwitch", 0, toggle ? 0 : 0.9f);
+
+        SoundPlayer.quickStart(toggle ? "Sounds/switchOn" : "Sounds/switchOff");
         /*
         if (toggle) { OnToggleOn(); }
         else { OnToggleOff(); }
